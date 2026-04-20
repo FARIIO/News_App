ae96731238773fa0ce6cf4320c6b21d0ca75f10c
fariio <fares.ahmed7474@gmail.com>
2026-04-20 06:17:56 +0200

    + Create App Text Style + implement To App theme

---
+++ lib/utils/app_text_style.dart
@@ -0,0 +1,68 @@
+import 'dart:ui';
+import 'package:elyoum_elsa2e3/utils/app_colors.dart';
+import 'package:flutter/src/painting/text_style.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+  static TextStyle med14White = GoogleFonts.inter(
+    color: AppColors.white,
+    fontWeight: FontWeight.w500,
+    fontSize: 14
+  );
+  static TextStyle med12White = GoogleFonts.inter(
+      color: AppColors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 12
+  );
+  static TextStyle med12Black = GoogleFonts.inter(
+      color: AppColors.black,
+      fontWeight: FontWeight.w500,
+      fontSize: 12
+  );
+  static TextStyle med14Black = GoogleFonts.inter(
+      color: AppColors.black,
+      fontWeight: FontWeight.w500,
+      fontSize: 14
+  );
+  static TextStyle med20Black = GoogleFonts.inter(
+      color: AppColors.black,
+      fontWeight: FontWeight.w500,
+      fontSize: 20
+  );
+  static TextStyle med20White = GoogleFonts.inter(
+      color: AppColors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 20
+  );
+  static TextStyle bold16White = GoogleFonts.inter(
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 16
+  );
+  static TextStyle bold24White = GoogleFonts.inter(
+      color: AppColors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 24
+  );
+  static TextStyle bold24Black = GoogleFonts.inter(
+      color: AppColors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 24
+  );
+  static TextStyle med24White = GoogleFonts.inter(
+      color: AppColors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 24
+  );
+  static TextStyle med24Black = GoogleFonts.inter(
+      color: AppColors.black,
+      fontWeight: FontWeight.w500,
+      fontSize: 24
+  );
+  static TextStyle bold16Black = GoogleFonts.inter(
+      color: AppColors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 16
+  );
+
+}
